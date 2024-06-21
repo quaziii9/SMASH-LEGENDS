133809c185370d7c6210ecab962ec1e12a4f9373
quaziii9 <quaziii9gamedev@gmail.com>
2024-06-21 11:22:59 +0900

    [BugFix] Start버튼 누르면 client로 들어가게
    - !NetworkClient.isConnected 시 starthost
    - 콤보 공격중 마지막 공격후 공격 버튼 클릭시 이동하는 문제 해결

--- Assets/02.Scripts/RoomManager.cs
+++ Assets/02.Scripts/RoomManager.cs
@@ -17,6 +17,11 @@ public class RoomManager : NetworkRoomManager
             Destroy(gameObject);
         }
     }
+    public void Update()
+    {
+        Debug.Log(roomSlots.Count);
+        Debug.Log(numPlayers);
+    }
 
     public override void OnStartHost()
     {
