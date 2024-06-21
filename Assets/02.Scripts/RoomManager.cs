d38ccca4812065b64d8e93c5407aa60a78115c55
quaziii9 <quaziii9gamedev@gmail.com>
2024-06-21 16:35:55 +0900

    [Bug fix] 플레이어가 2명다 입장했을시 자동으로 게임룸 입장 오류 해결
    - 플레이어가 다 입장하면 대기후 게임씬으로 넘어가게함 너무 빨리 넘어가서 오류 발생
    
    [Bug] 플레이어가 원하는 위치에서 스폰되지않음

--- Assets/02.Scripts/RoomManager.cs
+++ Assets/02.Scripts/RoomManager.cs
@@ -6,7 +6,7 @@ using Cysharp.Threading.Tasks;
 public class RoomManager : NetworkRoomManager
 {
     public static RoomManager Instance;
-
+    private bool isSceneChanging = false;  // 씬 전환 상태를 추적하는 플래그
     private void Awake()
     {
         if (Instance == null)
@@ -23,15 +23,13 @@ public class RoomManager : NetworkRoomManager
     public override void OnStartHost()
     {
         base.OnStartHost();
-        Debug.Log("Host started");
     }
 
     public override void OnRoomServerConnect(NetworkConnectionToClient conn)
     {
         base.OnRoomServerConnect(conn);
-        Debug.Log("Player connected: " + conn.connectionId);
 
-       // WaitToStartGame().Forget();
+       WaitToStartGame().Forget();
     }
 
     public override void OnRoomServerAddPlayer(NetworkConnectionToClient conn)
@@ -49,9 +47,22 @@ public class RoomManager : NetworkRoomManager
 
     private async UniTaskVoid WaitToStartGame()
     {
-        await UniTask.Delay(1500); // 1.5초 대기
-        if (numPlayers == maxConnections)
+        if (numPlayers < maxConnections)
         {
+            // 모든 플레이어가 연결될 때까지 대기
+            while (numPlayers < maxConnections)
+            {
+                await UniTask.Yield();
+            }
+        }
+
+        // 모든 플레이어가 연결된 후에 1.5초 대기
+        await UniTask.Delay(1500);
+
+        // 플레이어 수가 최대 연결 수에 도달하면 게임 씬으로 전환
+        if (!isSceneChanging && numPlayers == maxConnections)
+        {
+            isSceneChanging = true;  // 씬 전환 시작
             ServerChangeScene(GameplayScene);
         }
     }
@@ -73,16 +84,20 @@ public class RoomManager : NetworkRoomManager
         NetworkStartPosition[] startPositions = FindObjectsOfType<NetworkStartPosition>();
         Transform startPosition1 = null;
         Transform startPosition2 = null;
+   
 
         foreach (NetworkStartPosition pos in startPositions)
         {
             if (pos.name == "NetworkStartPosition1")
             {
                 startPosition1 = pos.transform;
+                Debug.Log(startPosition1.transform.position);
             }
             else if (pos.name == "NetworkStartPosition2")
             {
                 startPosition2 = pos.transform;
+                Debug.Log(startPosition2.transform.position);
+
             }
         }
 
@@ -95,7 +110,6 @@ public class RoomManager : NetworkRoomManager
         NetworkConnectionToClient[] connections = new NetworkConnectionToClient[NetworkServer.connections.Values.Count];
         NetworkServer.connections.Values.CopyTo(connections, 0);
 
-        Debug.Log(connections.Length);
         if (connections.Length == 1)
         {
             NetworkConnectionToClient hostConnection = connections[0];
@@ -104,15 +118,13 @@ public class RoomManager : NetworkRoomManager
                 hostConnection.identity.transform.position = startPosition1.position;
             }
         }
-        Debug.Log(connections.Length);
+        
         if (connections.Length == 2)
         {
-            //NetworkConnectionToClient clientConnection = connections[1];
-            //if (clientConnection.identity != null)
-            //{
-                connections[0].identity.transform.position = startPosition1.position;
-                connections[1].identity.transform.position = startPosition2.position;
-            //}
+            Debug.Log(startPosition1.position);
+            Debug.Log(startPosition2.position);
+            connections[0].identity.transform.position = startPosition1.position;
+            connections[1].identity.transform.position = startPosition2.position;
         }
     }
 }
