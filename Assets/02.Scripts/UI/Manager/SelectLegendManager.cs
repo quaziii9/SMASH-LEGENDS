9666bdee1bd473555a95dcf6439b9fb6a72a95c6
eunjoo <quaziii9gamedev@gmail.com>
2024-10-02 00:23:31 +0900

    [feat] 하위에 오브젝트 배치 - 버튼 누르면 바뀌게끔 설정해야함

--- Assets/02.Scripts/UI/Manager/SelectLegendManager.cs
+++ Assets/02.Scripts/UI/Manager/SelectLegendManager.cs
@@ -29,18 +29,45 @@ public class SelectLegendManager : Singleton<SelectLegendManager>
     public void SelectSkinUIOnEnable()
     {
         SelectSkinUI.SetActive(true);
+
         switch (SkinLegendType)
         {
             case LegendType.Peter:
-                PeterSkin.SetActive(true);
-                HookSkin.SetActive(false);
+                ActivateFirstChild(PeterSkin);  // PeterSkin의 0번째 자식만 활성화
+                DeactivateAllChildren(HookSkin);  // HookSkin의 모든 자식 비활성화
                 break;
             case LegendType.Hook:
-                PeterSkin.SetActive(false);
-                HookSkin.SetActive(true);
+                ActivateFirstChild(HookSkin);  // HookSkin의 0번째 자식만 활성화
+                DeactivateAllChildren(PeterSkin);  // PeterSkin의 모든 자식 비활성화
                 break;
         }
     }
+
+    // 특정 오브젝트의 0번째 자식만 활성화하는 함수
+    private void ActivateFirstChild(GameObject parent)
+    {
+        parent.SetActive(true);
+        if (parent.transform.childCount > 0)
+        {
+            for (int i = 0; i < parent.transform.childCount; i++)
+            {
+                if (i == 0)
+                    parent.transform.GetChild(i).gameObject.SetActive(true);  // 0번째 자식 활성화
+                else
+                    parent.transform.GetChild(i).gameObject.SetActive(false); // 나머지 비활성화
+            }
+        }
+    }
+
+    // 특정 오브젝트의 모든 자식들을 비활성화하는 함수
+    private void DeactivateAllChildren(GameObject parent)
+    {
+        for (int i = 0; i < parent.transform.childCount; i++)
+        {
+            parent.transform.GetChild(i).gameObject.SetActive(false);  // 모든 자식 비활성화
+        }
+    }
+
 
     public void SelectSkinUIOnDisable()
     {
